f8e3e471548f894e987ec69eada9f52faf56e8b5
Bhimsen Mokale <web.bhimsen05@gmail.com>
2021-07-31 23:05:57 +0530

    configure desktop layout

---
+++ assets/js/scripts.js
@@ -0,0 +1,28 @@
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 20) {
+
+        document.getElementById("navbar").style.background = "rgba(255,255,255, 0.9)";
+        document.getElementById("brand").style.padding = "0.1rem 0";
+        document.getElementById("menu").style.top = "6.2rem"
+        document.getElementById("navbar-checkbox").style.top = "1rem"
+
+    }
+    //  else if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    // }
+    else {
+        document.getElementById("navbar").style.background = "none";
+        document.getElementById("brand").style.padding = "1rem 0";
+        document.getElementById("menu").style.top = "8.2rem"
+        document.getElementById("navbar-checkbox").style.top = "2.2rem"
+    }
+}
+
+document.querySelector('video').playbackRate = "0.00001";
+
+
+
+function uncheckBox() {
+    document.getElementById('navbar-checbox').checked = false
+}
