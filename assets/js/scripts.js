7152eabb479896c7b5292dc3c57bcf11e8d08ad1
Elliot Alderson <web.bhimsen05@gmail.com>
2021-08-02 08:56:37 +0530

    configure tablet layout and adding little animations

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,8 +5,6 @@ function scrollFunction() {
 
         document.getElementById("navbar").style.background = "rgba(255,255,255, 0.9)";
         document.getElementById("brand").style.padding = "0.1rem 0";
-        document.getElementById("menu").style.top = "6.2rem"
-        document.getElementById("navbar-checkbox").style.top = "1rem"
 
     }
     //  else if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -14,12 +12,8 @@ function scrollFunction() {
     else {
         document.getElementById("navbar").style.background = "none";
         document.getElementById("brand").style.padding = "1rem 0";
-        document.getElementById("menu").style.top = "8.2rem"
-        document.getElementById("navbar-checkbox").style.top = "2.2rem"
     }
 }
-
-document.querySelector('video').playbackRate = "0.00001";
 
 
 
